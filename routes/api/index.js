c141adc721337af2d0202e50f80388636a2fab2e
Rooster <gallolopez1@hotmail.com>
2021-12-09 21:39:45 -0500

    Started post request and used UUID

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,14 @@
 const router = require('express').Router();
 const notes = require('../../db/db.json');
+const addNote = require('../../lib/note');
 
 router.get('/notes', (req, res) => {
     let results = notes;
     res.json(results);
 })
 
+router.post('/notes', (req, res) => {
+    addNote(req.body, notes);
+})
 
 module.exports = router;
