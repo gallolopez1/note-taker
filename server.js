7fa8ae8e00f2c451160de2e3737fe0de8824fe3e
Rooster <gallolopez1@hotmail.com>
2021-12-09 20:31:04 -0500

    Created GET request for notes

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
-const html = require('./routes/html');
+const html = require('./routes/html/index');
+const api = require('./routes/api/index');
 
 const app = express();
+// use throughout all files in public folder
 app.use(express.static('public'));
-
+// allows us to return the json format
+app.use(express.json());
+app.use('/api', api);
+// when navigating to homepage, show index.html
 app.use('/', html);
 
-app.listen(3001, () => console.log(`API IS DONE`));
+// console log when user hits server route
+app.listen(PORT, () => {
+    console.log(`HTML IS DONE`);
+});
